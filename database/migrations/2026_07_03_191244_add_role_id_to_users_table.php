f2d9df7f1d4cb2b7adefaf16356efb631cd7d7d1
Eugene Kutuzov <ekutuzov@itera-research.com>
2026-07-03 22:57:38 +0300

    .

--- database/migrations/2026_07_03_191244_add_role_id_to_users_table.php
+++ database/migrations/2026_07_03_191244_add_role_id_to_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->foreignId('role_id')
                 ->nullable()
-                ->after('role_id')
+                ->after('department_id')
                 ->constrained()
                 ->nullOnDelete();
         });
