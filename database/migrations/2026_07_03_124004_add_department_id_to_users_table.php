cb85013165dcaea7ca7daea8a30e08bf274aa524
Eugene Kutuzov <ekutuzov@itera-research.com>
2026-07-03 16:00:31 +0300

    .

--- database/migrations/2026_07_03_124004_add_department_id_to_users_table.php
+++ database/migrations/2026_07_03_124004_add_department_id_to_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->foreignId('department_id')
                 ->nullable()
-                ->after('role')
+                ->after('email')
                 ->constrained()
                 ->nullOnDelete();
         });
