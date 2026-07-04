57242d97fd4ff97d9de3753e0ba49647b8183bf3
Eugene Kutuzov <ekutuzov@itera-research.com>
2026-07-04 18:13:09 +0300

    tickets in main menu

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -39,6 +39,12 @@ const showingNavigationDropdown = ref(false);
                                 >
                                     Dashboard
                                 </NavLink>
+                                <NavLink
+                                    :href="route('tickets')"
+                                    :active="route().current('dashboard')"
+                                >
+                                    Tickets
+                                </NavLink>
                             </div>
                         </div>
 
