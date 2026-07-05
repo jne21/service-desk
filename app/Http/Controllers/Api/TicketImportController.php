4275937860a97b9556acf619691df484cd4f18e8
Eugene Kutuzov <ekutuzov@itera-research.com>
2026-07-05 16:22:45 +0300

    TicketImportController

--- app/Http/Controllers/Api/TicketImportController.php
+++ app/Http/Controllers/Api/TicketImportController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\TicketSource;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class TicketImportController extends Controller
 {
-    //
+    public function store(Request $request): JsonResponse
+    {
+        /** @var TicketSource $source */
+        $source = $request->attributes->get('ticket_source');
+
+        return response()->json([
+            'success' => true,
+            'source' => [
+                'id' => $source->id,
+                'code' => $source->code,
+                'name' => $source->name,
+            ],
+        ]);
+    }
 }
