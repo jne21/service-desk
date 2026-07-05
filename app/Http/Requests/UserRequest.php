185c4288830cc164086a77bb44c29518ece755e5
Eugene Kutuzov <ekutuzov@itera-research.com>
2026-07-05 12:11:45 +0300

    pw in user request

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UserRequest extends FormRequest
 {
@@ -13,10 +14,18 @@ class UserRequest extends FormRequest
 
     public function rules(): array
     {
+        $user = $this->route('user');
+
         return [
             'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique('users', 'email')->ignore($user)],
             'department_id' => ['required', 'exists:user_departments,id'],
             'role_id' => ['required', 'exists:user_roles,id'],
+            'password' => [
+                $this->isMethod('post') ? 'required' : 'nullable',
+                'string', 'min:8',
+                'confirmed',
+            ],
         ];
     }
 }
