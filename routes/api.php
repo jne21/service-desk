844c41cff01fd80f045602f092cb4d540da46385
Eugene Kutuzov <ekutuzov@itera-research.com>
2026-07-06 18:04:16 +0300

    user/tickets route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
-use App\Http\Controllers\Api\TicketImportController;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\TicketImportController;
+use App\Http\Controllers\Api\UserTicketController;
 
 Route::middleware(['ticket-source'])->group(function () {
     Route::post('/tickets/import', [TicketImportController::class, 'store']);
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user/tickets', [UserTicketController::class, 'index']);
 });
