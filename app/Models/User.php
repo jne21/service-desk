4697283f696473a5ecb62a596838513b4816c6cf
Eugene Kutuzov <ekutuzov@itera-research.com>
2026-07-06 17:21:31 +0300

    user tokenable

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
@@ -19,7 +19,8 @@ class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
-
+    use HasApiTokens;
+    
     private const ROLE_ADMIN_ID = 1;
 
     protected $fillable = [
