9d91020931ae5c2b9924781c93b725cd0466c956
Eugene Kutuzov <ekutuzov@itera-research.com>
2026-07-06 20:01:54 +0300

    humanize error messages

--- app/Http/Requests/Api/UserTicketIndexRequest.php
+++ app/Http/Requests/Api/UserTicketIndexRequest.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Requests\Api;
 
-use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UserTicketIndexRequest extends FormRequest
@@ -19,6 +19,18 @@ class UserTicketIndexRequest extends FormRequest
             'ticket_id' => ['nullable', 'integer', 'min:1'],
             'date_from' => ['nullable', 'date'],
             'date_to' => ['nullable', 'date', 'after_or_equal:date_from'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'ticket_id.integer' => 'Параметр ticket_id має бути числом.',
+            'ticket_id.min' => 'Параметр ticket_id має бути більше 0.',
+
+            'date_from.date' => 'Параметр date_from має бути коректною датою.',
+            'date_to.date' => 'Параметр date_to має бути коректною датою.',
+            'date_to.after_or_equal' => 'Параметр date_to має бути не раніше date_from.',
         ];
     }
 
