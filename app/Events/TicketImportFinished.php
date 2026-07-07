ea4eac143d88e7ba8521f6e965ac0b762c120379
Eugene Kutuzov <ekutuzov@itera-research.com>
2026-07-07 11:32:56 +0300

    TicketImportFinished event

--- app/Events/TicketImportFinished.php
+++ app/Events/TicketImportFinished.php
@@ -2,35 +2,19 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use App\Models\TicketSource;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class TicketImportFinished
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable, SerializesModels;
 
-    /**
-     * Create a new event instance.
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, Channel>
-     */
-    public function broadcastOn(): array
-    {
-        return [
-            new PrivateChannel('channel-name'),
-        ];
+    public function __construct(
+        public TicketSource $source,
+        public int $created,
+        public int $updated,
+        public float $duration,
+    ) {
     }
 }
