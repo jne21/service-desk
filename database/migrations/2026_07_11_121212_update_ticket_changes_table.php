72214cf6522dd9f91150381b16847cc49a273f7e
Eugene Kutuzov <ekutuzov@itera-research.com>
2026-07-11 13:10:01 +0300

    extend ticket_changes table

--- database/migrations/2026_07_11_121212_update_ticket_changes_table.php
+++ database/migrations/2026_07_11_121212_update_ticket_changes_table.php
@@ -6,44 +6,54 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('ticket_changes', function (Blueprint $table) {
+            if (! Schema::hasColumn('ticket_changes', 'ticket_id')) {
+                $table->foreignId('ticket_id')
+                    ->constrained('tickets')
+                    ->cascadeOnDelete();
+            }
 
-            $table->foreignId('ticket_id')
-                ->constrained('tickets')
-                ->cascadeOnDelete();
+            if (! Schema::hasColumn('ticket_changes', 'user_id')) {
+                $table->foreignId('user_id')
+                    ->nullable()
+                    ->constrained('users')
+                    ->nullOnDelete();
+            }
 
-            $table->foreignId('user_id')
-                ->nullable()
-                ->constrained('users')
-                ->nullOnDelete();
+            if (! Schema::hasColumn('ticket_changes', 'ticket_source_id')) {
+                $table->foreignId('ticket_source_id')
+                    ->nullable()
+                    ->constrained('ticket_sources')
+                    ->nullOnDelete();
+            }
 
-            $table->foreignId('ticket_source_id')
-                ->nullable()
-                ->constrained('ticket_sources')
-                ->nullOnDelete();
+            if (! Schema::hasColumn('ticket_changes', 'event')) {
+                $table->string('event', 50);
+            }
 
-            $table->string('event', 50);
+            if (! Schema::hasColumn('ticket_changes', 'changes')) {
+                $table->json('changes')->nullable();
+            }
+        });
 
-            $table->json('changes')->nullable();
+        Schema::table('ticket_changes', function (Blueprint $table) {
+            if (! Schema::hasIndex(
+                'ticket_changes',
+                ['ticket_id', 'created_at']
+            )) {
+                $table->index(['ticket_id', 'created_at']);
+            }
 
-            $table->timestamps();
-
-            $table->index(['ticket_id', 'created_at']);
-            $table->index('event');
-
+            if (! Schema::hasIndex('ticket_changes', 'event')) {
+                $table->index('event');
+            }
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('ticket_changes');
+  	//
     }
 };
